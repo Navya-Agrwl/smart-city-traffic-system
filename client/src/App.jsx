a881251c09a39e9fe7ff6ce6d523c8ece9cd49b6
Navya Aggarwal <navya04114@gmail.com>
2026-05-26 10:49:04 +0530

    Fixed client folder issue

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Dashboard from "./pages/Dashboard"
+
+function App() {
+  return (
+    <div className="bg-[#0f172a] min-h-screen p-6">
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return (
+    <div className="text-white text-3xl">
+      Dashboard
+    </div>
+  )
+}
+
+export default Dashboard
